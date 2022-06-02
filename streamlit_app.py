82f7d716d443de9ac0ad7d17f932a0fd78fb80a6
jrvollmer <72111610+jrvollmer@users.noreply.github.com>
2022-06-02 09:32:05 -0500

    Update streamlit_app.py
    Import panda and read csv file from S3 bucket from DWW course
    Change text to match course so the data will make sense when used

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,14 @@
 import streamlit
+import pandas
 
-streamlit.title('First Streamlit App')
-streamlit.header('☺️Some Header Text')
-streamlit.text('😎Some regular text')
-streamlit.text('😏More regular text')
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlist.dataframe(my_fruit_list)
+
+streamlit.title('My Parents\' New Healthy Diner')
+streamlit.header('Breakfast Favorites')
+streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞Avocado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
