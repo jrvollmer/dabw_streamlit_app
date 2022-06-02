1d5d0360d225f31e896c69aa6d0c14cbae35a70f
jrvollmer <72111610+jrvollmer@users.noreply.github.com>
2022-06-02 09:24:39 -0500

    Update streamlit_app.py
    Emojis... why not

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 
 streamlit.title('First Streamlit App')
-streamlit.header('Some Header Text')
-streamlit.text('Some regular text')
-streamlit.text('More regular text')
+streamlit.header('☺️Some Header Text')
+streamlit.text('😎Some regular text')
+streamlit.text('😏More regular text')
