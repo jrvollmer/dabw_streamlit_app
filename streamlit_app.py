728d693697032cd7debb0e7dfe9a0e620b6c8a96
jrvollmer <72111610+jrvollmer@users.noreply.github.com>
2022-06-02 09:34:52 -0500

    Update streamlit_app.py
    Change order

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,14 +1,15 @@
 import streamlit
 import pandas
 
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-streamlit.dataframe(my_fruit_list)
-
 streamlit.title('My Parents\' New Healthy Diner')
 streamlit.header('Breakfast Favorites')
 streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞Avocado Toast')
-
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+# Pull csv from S3 bucket into my_fruit_list dataframe
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+# Display data on the page
+streamlit.dataframe(my_fruit_list)
