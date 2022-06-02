0e1458beeb4070221d0d632817f3744124b0f7a1
jrvollmer <72111610+jrvollmer@users.noreply.github.com>
2022-06-02 15:56:26 -0500

    Typo. Also, forgot to mention variable input before

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,7 +27,7 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityyvice Fruit Advice (Maybe?)!")
 
 fruit_choice = streamlit.text_input('What fruit would you like information about?', 'Kiwi')
-stramlit.write('The user entered', fruit_choice)
+streamlit.write('The user entered', fruit_choice)
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
