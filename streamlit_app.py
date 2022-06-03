581dccce569091eaefdfeee0ccba70ce1f15794f
jrvollmer <72111610+jrvollmer@users.noreply.github.com>
2022-06-03 09:34:58 -0500

    Typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,7 +31,7 @@ def get_fruityvice_data(this_fruit_choice):
   fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
   # Take the json version of the response and normalize it
   fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-  return fruityvice_normlized
+  return fruityvice_normalized
 
 
 # Fruityvice API call section
